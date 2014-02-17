6d63d064df6d1156a8a11f9f78ccd523b3c425b2
Raleigh Fatoki <fatoki09@yahoo.com>
2014-02-17 00:14:02 -0600

    basic patient functionality in place

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SenegalRegistration::Application.config.secret_key_base = 'f1fb06e358231604f6f03ef6eba0eb98e0fee3c2b6bcc225525a685eb8907c1e9143a84016f54afdf0e88f27deec9c7f4ace4dac7c9ec8b7255482c4fb7253b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SenegalRegistration::Application.config.session_store :cookie_store, key: '_senegal-registration_session'
